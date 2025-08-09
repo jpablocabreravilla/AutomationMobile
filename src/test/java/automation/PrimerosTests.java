988759c68c0761386d05a24323632a0dcb282189
jpablocabreravilla <jpablocabreravilla@gmail.com>
2025-08-09 14:05:13 -0500

    Base project

---
+++ src/test/java/automation/PrimerosTests.java
@@ -0,0 +1,14 @@
+package automation;
+
+import org.testng.annotations.Test;
+import utilities.BaseTest;
+import utilities.Logs;
+
+public class PrimerosTests extends BaseTest {
+
+    @Test
+    public void primerTest(){
+        Logs.info("Mi primer test");
+    }
+
+}
--- src/test/java/utilities/BaseTest.java
+++ src/test/java/utilities/BaseTest.java
@@ -1,25 +1,63 @@
 package utilities;
 
+import io.appium.java_client.android.AndroidDriver;
 import listeners.SuiteListeners;
 import listeners.TestListeners;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.asserts.SoftAssert;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 @Listeners({TestListeners.class, SuiteListeners.class})
 public class BaseTest {
+
     protected SoftAssert softAssert;
-    protected final String smoke = "smoke";
     protected final String regression = "regression";
+    protected final String smoke = "smoke";
+    protected AndroidDriver driver;
 
     @BeforeMethod(alwaysRun = true)
     public void masterSetUp() {
         softAssert = new SoftAssert();
+        Logs.debug("Inicializando el driver");
+        driver = initDriver();
     }
 
     @AfterMethod(alwaysRun = true)
     public void masterTearDown() {
-
+        Logs.debug("Matando al driver");
+        driver.quit();
     }
+
+    private static AndroidDriver initDriver() {
+        try {
+            final var appiumUrl = "http://127.0.0.1:4723/";
+            final var desiredCapabilities = getDesiredCapabilities();
+
+            Logs.debug("Inicializando el driver");
+            return new AndroidDriver(new URL(appiumUrl), desiredCapabilities);
+        } catch (MalformedURLException malformedURLException) {
+            Logs.error("Error al inicializar el driver: %s", malformedURLException.getLocalizedMessage());
+            throw new RuntimeException();
+        }
+    }
+
+    private static DesiredCapabilities getDesiredCapabilities() {
+        final var desiredCapabilities = new DesiredCapabilities();
+        final var fileAPK = new File("src/test/resources/apk/sauceLabs.apk");
+
+        desiredCapabilities.setCapability("appium:autoGrantPermissions", true);
+        desiredCapabilities.setCapability("appium:appWaitActivity", "com.swaglabsmobileapp.MainActivity");
+        desiredCapabilities.setCapability("appium:platformName", "android");
+        desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
+        desiredCapabilities.setCapability("appium:app", fileAPK.getAbsolutePath());
+
+        return desiredCapabilities;
+    }
+
 }
